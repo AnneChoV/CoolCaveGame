459c1a109ca9f516cafd192554319c60324f4c7c
Anne <miss.anne.c@hotmail.com>
2017-03-13 16:06:18 +1300

    Raycast to object
    Trying to do glowy outline

---
+++ Cool Cave Game/Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        Vector3 inFront = transform.TransformDirection(Vector3.forward);
+
+        if (Physics.Raycast(ray, out hit, 1.5f))
+        {
+            if (hit.transform.tag == "InteractableItem")
+            {
+                Debug.Log("There's something infront of the player!");
+                hit.transform.SendMessage("HitByRay");
+            }       
+        }
+
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "InteractableItem")
+        {
+            Debug.Log("Collided with stuff");
+        }
+    }
+}
