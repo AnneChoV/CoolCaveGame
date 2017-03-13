7cf70015358d210fcefb180e2406e5293301f5dd
Amber <amberleejulian@gmail.com>
2017-03-13 17:07:10 +1300

    Pick up object
    like 2 lines of code wow

--- Cool Cave Game/Assets/Scripts/Player.cs
+++ Cool Cave Game/Assets/Scripts/Player.cs
@@ -4,8 +4,14 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    //The one pickupable item
+    GameObject cat;
+    [Header("Game Item Bools")]
+    public bool hasGotCat;
+
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
@@ -16,12 +22,14 @@ public class Player : MonoBehaviour {
 
         Vector3 inFront = transform.TransformDirection(Vector3.forward);
 
-        if (Physics.Raycast(ray, out hit, 1.5f))
+        if (Physics.Raycast(ray, out hit, 1.5f))    
         {
-            if (hit.transform.tag == "InteractableItem")
+            if (hit.transform.tag == "InteractableItem") //Another way to do it would be for all interactable items to have the same shared parent, check for this parent and then have specific tags for each one to check for.
             {
                 Debug.Log("There's something infront of the player!");
                 hit.transform.SendMessage("HitByRay");
+                hasGotCat = true;
+                hit.transform.gameObject.SetActive(false);
             }       
         }
 
