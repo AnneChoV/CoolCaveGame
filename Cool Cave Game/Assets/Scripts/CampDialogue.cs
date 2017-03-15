46f2bdf4ca213b27b939e9b2ed550ff925e03164
Anne <miss.anne.c@hotmail.com>
2017-03-16 10:07:43 +1300

    Dialogue things

---
+++ Cool Cave Game/Assets/Scripts/CampDialogue.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+public class CampDialogue : MonoBehaviour {
+
+    public Flowchart flowChart;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            flowChart.SetBooleanVariable("Intervene", true);
+        }
+        
+	}
+}
