379d7ce7dd9950a926dbda541be079df1b65353d
Amber <amberleejulian@gmail.com>
2017-03-20 17:43:03 +1300

    Changed physics layer of some stuff, created inventory kind of

--- Cool Cave Game/Assets/Scripts/Player.cs
+++ Cool Cave Game/Assets/Scripts/Player.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
+    
 
 	// Use this for initialization
 	void Start () {
---
+++ Cool Cave Game/Assets/Scripts/PlayerInventory.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInventory : MonoBehaviour {  //THIS ENTIRE SCRIPT IS TO TAKE THE SEPERATE INVENTORY PIECES OUT OF THE PLAYER
+
+    public List<bool> woodPieces;
+    public bool allWoodPiecesCollected;
+
+	void Start () {
+        foreach (Transform child in transform)  //NEED TO MAKE THIS THE PARENT TRANSFORM THAT HOLDS ALL THE WOOD.
+        {
+            woodPieces.Add(false);
+        }
+	}
+	
+	void Update () {
+		
+	}
+
+    //If a woodpiece is added to the inventory, one of the bools in the list should be made true.
+    //If all the bools in the list are true, set AllWoodPeicesCollected to true.
+}
